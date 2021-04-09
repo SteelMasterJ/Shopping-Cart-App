2585561b81ed7699fe17e5f18b74b0182736501a
Joseph Steel <62897806+SteelMasterJ@users.noreply.github.com>
2021-04-09 11:43:55 -0400

    Configured TailwindCSS

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-green-500">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-green-500"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
